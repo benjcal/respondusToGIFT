ea78ca24ed81e030f0bf1da99a7e37f539bdbb81
Benjamin Calderon <benj.calderon@gmail.com>
2015-05-22 12:11:15 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # respondusToGIFT
+
+The goal of this project is provide a convenient way to convert standard Respondus quiz files to Moodle GIFT format.
---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("Hello!");
+
+// This is supposed to be an array of objects, each object will be a question
+var questions[]
+
+
+// open the file
+function openFile (argument) {
+	// body...
+}
+
+function detectQuestionStart (argument) {
+	// body...
+}
+
+function addToQuestion (argument) {
+	// body...
+}
+
+
+// 
