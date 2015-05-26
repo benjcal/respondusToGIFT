08baf54dd5981f43e6099cf3545fefda6419f1ba
Benjamin Calderon <benj.calderon@gmail.com>
2015-05-26 12:08:20 -0400

    more fixings

--- main.js
+++ main.js
@@ -41,7 +41,6 @@ String.prototype.removeExtraSpace = function() {
 
 var fs = require('fs');
 
-//process.argv[2] is the first argument
 // var inputFile = fs.readFileSync(process.argv[2], 'utf8');
 var inputFile = fs.readFileSync("sampleQuestion.txt", 'utf8');
 
@@ -57,6 +56,7 @@ function linesToArray(str) { // separate input string into an array of lines
             line = "";
         }
     }
+    // remove empty array elements
     for (var i = 0; i < tempLines.length; i++) {
         if (!(/\s/.test(tempLines[i]))) {
             continue;
@@ -68,77 +68,38 @@ function linesToArray(str) { // separate input string into an array of lines
     return lines;
 }
 
-// function parseLines(linesArray) {
-//
-//     // var isTitle = /(\d|\d\d)(\.|\))/; // match the question number, ej. '3)' or '64.'
-//     // var isAnswer = /[a-z](\)|\.)/i; // match answers, ej. 'a.' or 'D)'
-//     // var isCorrectAnswer = /\*[a-z](\)|\.)/i; // match correct answer(s), ej. '*B.' or '*a)'
-//
-//     var str = [];
-//
-//     for (var i = 0; i <= linesArray.length - 1; i++) {
-//         if (linesArray[i].isNumber()) {
-//
-//             str.push(linesArray[i]);
-//
-//             // console.log(linesArray[i] + '\tis title');
-//         }
-//         if (linesArray[i].isLetter()) {
-//             // console.log(linesArray[i] + '\tis answer');
-//         }
-//         if (linesArray[i].isCorrect()) {
-//             // console.log(linesArray[i] + '\tis correct answer');
-//         }
-//
-//     }
-//     // console.log();
-// }
+function question(number, type, title, answers) {
+    this.questionNumber = number;
+    this.questionType = type;
+    this.questionTitle = title;
+    this.questionAnswers = answers;
+}
+
+// Function to indentify the question type
+// MC = Multiple Choice
+// TF = True/False
+// MR = Multiple Response/Answer
+// FB = Fill in the Blank/Short Answer
+// ES = Essay/Paragraph
+function questionType() {
+
+}
 
 
 
-var linesArray = linesToArray(inputFile);
+var questions = [];
+
+questions.push(new question(1, "MC", "Who's the coolest?", ["none", "to much proudness here", "such person doesn't exist", "lol"]));
+questions.push(new question(2, "TF", "Is there a moon?", ["maybe?", "who's \"a moon\"", "meh..."]));
+
+console.log(questions);
 
 
-console.log(linesArray);
+// var linesArray = linesToArray(inputFile);
+//
+// console.log(linesArray);
 
 
 
 
 // fs.writeFileSync(process.argv[2] += '.gift', "Hello!");
-
-
-
-// console.log(s.slice(y["index"],s.length - 1));
-// console.log(s.slice(y[0].length, s.length)); // print the string minus the length of the RegEx match
-
-// for (var i = 0; i <= array.length - 1; i++) {
-// removeSpace(linesToArray(sampleQuestion)[]);
-// };
-
-
-// Function to extract the parts of the questions
-// function parseQuestion() {
-// body...
-// }
-
-
-// this is a constructor for the questions
-
-// function question(type, number, body, answers) {
-//     this.questionType = type;
-//     //	0: Multiple Choice, 1: True/False, 2: Essay, 3: Matching
-//     //	4: Short Answers
-//     this.questionNumber = number; // Number of the question
-//     this.questionBody = body; // The question body
-//     this.questionAnswers = answers;
-// }
-
-
-// var question4 = new question(0, 4, "What does IC stands for in electronics?", ["International Council", "Information Cartoon", "Integrated Circuit", "Interlooping Connector"]);
-
-
-// console.log(sampleQuestion);
-
-// console.log(question4.questionBody + "\n{\n" + "=" + question4.questionAnswers[0] + "\n=" + question4.questionAnswers[1]);
-
-// Function to extract the parts of the questions
